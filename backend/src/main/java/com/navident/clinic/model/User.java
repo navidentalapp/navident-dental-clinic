b78a1852e77757c3c656e517198d7adba909c29e
navidentalapp <navidentalpg@outlook.com>
2025-08-14 08:54:06 +0530

    Update User.java

--- backend/src/main/java/com/navident/clinic/model/User.java
+++ backend/src/main/java/com/navident/clinic/model/User.java
@@ -49,6 +49,16 @@ public class User implements UserDetails {
     }
 
     @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
     public boolean isAccountNonExpired() {
         return accountNonExpired;
     }
