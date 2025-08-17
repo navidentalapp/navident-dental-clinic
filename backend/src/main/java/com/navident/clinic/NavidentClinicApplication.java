a7723810a9e90638f90e4e52f524ad1aee21dc45
naveenthaan <navidentalpg@outlook.com>
2025-08-17 15:42:36 +0530

    Actual Final-Good Working fix to use Mongodb URI and other parameters

--- backend/src/main/java/com/navident/clinic/NavidentClinicApplication.java
+++ backend/src/main/java/com/navident/clinic/NavidentClinicApplication.java
@@ -2,20 +2,19 @@ package com.navident.clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @SpringBootApplication
 public class NavidentClinicApplication {
 
     public static void main(String[] args) {
-        // Debug environment variables
-        String mongoUri = System.getenv("MONGODB_URI");
+        // Removed MongoDB URI debug prints to clean logs
         
-        if (mongoUri == null || mongoUri.contains("localhost")) {
-            System.err.println("WARNING: MongoDB URI not set properly!");
-        }
-
         SpringApplication.run(NavidentClinicApplication.class, args);
     }
+
+//String mongoUri = System.getenv("MONGODB_URI");
+//
+//if (mongoUri == null || mongoUri.contains("localhost")) {
+//    System.err.println("WARNING: MongoDB URI not set properly!");
+//
 }
